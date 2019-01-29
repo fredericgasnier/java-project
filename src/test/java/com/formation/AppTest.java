fcc5835a615d7ada441544735905d8ee230c83bb
fredericgasnier <fredo95100@gmail.com>
2019-01-29 14:54:02 +0100

    Third Commit

--- src/test/java/com/formation/AppTest.java
+++ src/test/java/com/formation/AppTest.java
@@ -10,7 +10,7 @@ public class AppTest {
 	@Test
 	public void testLength() {
 		Assert.assertEquals(64, App.sha256hex(INPUT).length());
-		System.out.println("Hello, code lancé pour la 2eme fois");
+		System.out.println("Hello World");
 	}
 
 	@Test
